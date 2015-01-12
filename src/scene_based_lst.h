da879e65489c07148ffbb195d09e97c42c63f234
Ronald Dilley <rdilley@usgs.gov>
2015-01-12 18:41:07 +0000

    Code format
    git-svn-id: https://edclxs131.cr.usgs.gov:18080/svn/lsrd/lst/trunk@1056 48de96a7-6a0d-487c-b84a-40161698abc1

--- src/scene_based_lst.h
+++ src/scene_based_lst.h
@@ -11,34 +11,34 @@
 #include "input.h"
 #include "error.h"
 
-int first_files
+int build_modtran_input
 (
-    Input_t *input,             /*I: input structure */
-    int *num_points,            /*O: number of NARR points */
-    bool verbose                /*I: value to indicate if intermediate messages 
-                                     be printed */
+    Input_t *input,          /*I: input structure */
+    int *num_points,         /*O: number of NARR points */
+    bool verbose             /*I: value to indicate if intermediate messages
+                                  be printed */
 );
 
 int second_narr
 (
-    Input_t *input,             /*I: input structure */
-    int num_points,             /*I: number of narr points */
-    float alb,                  /*I: albedo */ 
-    char **case_list,           /*I: modtran run list */
-    float **results,            /*O: atmospheric parameter for modtarn run */
-    bool verbose                /*I: value to indicate if intermediate messages 
-                                     be printed */
+    Input_t *input,          /*I: input structure */
+    int num_points,          /*I: number of narr points */
+    float alb,               /*I: albedo */
+    char **case_list,        /*I: modtran run list */
+    float **results,         /*O: atmospheric parameter for modtarn run */
+    bool verbose             /*I: value to indicate if intermediate messages
+                                  be printed */
 );
 
 int third_pixels_post
 (
-    Input_t *input,             /*I: input structure */
-    int num_points,             /*I: number of narr points */
-    char *dem_infile,           /*I: address of input DEM filename */
-    char *emi_infile,           /*I: address of input Emissivity filename */
-    float **modtran_results,    /*I: atmospheric parameter for modtarn run */
-    bool verbose                /*I: value to indicate if intermediate messages 
-                                     be printed */
+    Input_t *input,          /*I: input structure */
+    int num_points,          /*I: number of narr points */
+    char *dem_infile,        /*I: address of input DEM filename */
+    char *emi_infile,        /*I: address of input Emissivity filename */
+    float **modtran_results, /*I: atmospheric parameter for modtarn run */
+    bool verbose             /*I: value to indicate if intermediate messages
+                                  be printed */
 );
 
 #endif /* SCENE_BASED_LST_H */
