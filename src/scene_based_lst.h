54936ebd34554dced60fb6a750b6dbb09d22cfe4
Ronald Dilley <rdilley@usgs.gov>
2015-01-22 21:50:11 +0000

    Modified to use the struct and some other interface changes
    git-svn-id: https://edclxs131.cr.usgs.gov:18080/svn/lsrd/lst/trunk@1102 48de96a7-6a0d-487c-b84a-40161698abc1

--- src/scene_based_lst.h
+++ src/scene_based_lst.h
@@ -12,17 +12,25 @@
 
 #include "input.h"
 
+typedef struct
+{
+    char full_path[PATH_MAX];
+    float latitude;
+    float longitude;
+    float height;
+} CASE_POINT;
+
 
 int get_args
 (
-    int argc,                 /* I: number of cmd-line args */
-    char *argv[],             /* I: string of cmd-line args */
-    char **xml_infile,        /* I: address of input XML metadata filename  */
-    char **dem_infile,        /* I: address of input DEM filename */
-    char **emissivity_infile, /* I: address of input emissivity filename */
-    bool *tape_6,             /* O: use the tape6 output */
-    bool *verbose,            /* O: verbose flag */
-    bool *debug               /* O: debug flag */
+    int argc,                /* I: number of cmd-line args */
+    char *argv[],            /* I: string of cmd-line args */
+    char *xml_infile,        /* I: address of input XML metadata filename  */
+    char *dem_infile,        /* I: address of input DEM filename */
+    char *emissivity_infile, /* I: address of input emissivity filename */
+    bool *tape_6,            /* O: use the tape6 output */
+    bool *verbose,           /* O: verbose flag */
+    bool *debug              /* O: debug flag */
 );
 
 
@@ -31,7 +39,7 @@ int build_modtran_input
     Input_t *input,       /* I: input structure */
     int *num_pts,         /* O: number of NARR points */
     int *num_runs,        /* O: number of MODTRAN runs */
-    char ***case_list,    /* O: case list information (allocated here) */
+    CASE_POINT **case_list, /* O: case list information (allocated here) */
     char ***command_list, /* O: command list information (allocated here) */
     bool verbose,         /* I: value to indicate if intermediate messages
                                 will be printed */
@@ -45,7 +53,7 @@ int second_narr
     Input_t *input,   /* I: input structure */
     int num_points,   /* I: number of narr points */
     float alb,        /* I: albedo */
-    char **case_list, /* I: modtran run list */
+    CASE_POINT *case_list, /* I: modtran run list */
     float **results,  /* O: atmospheric parameter for modtarn run */
     bool verbose      /* I: value to indicate if intermediate messages will be
                             printed */
