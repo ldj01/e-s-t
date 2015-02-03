679d286e0894ee0f751d016be3c7277dda2183a4
Ronald Dilley <rdilley@usgs.gov>
2015-02-03 22:11:36 +0000

    Header file cleanup
    git-svn-id: https://edclxs131.cr.usgs.gov:18080/svn/lsrd/lst/trunk@1121 48de96a7-6a0d-487c-b84a-40161698abc1

---
+++ src/lst_types.h
@@ -0,0 +1,17 @@
+#ifndef LST_TYPES_H
+#define LST_TYPES_H
+
+
+#include <limits.h>
+
+
+typedef struct
+{
+    char full_path[PATH_MAX];
+    float latitude;
+    float longitude;
+    float height;
+} CASE_POINT;
+
+
+#endif /* LST_TYPES_H */
