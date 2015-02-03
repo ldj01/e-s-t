902027081c215dde6a230f75f15e4f923d6067d1
Ronald Dilley <rdilley@usgs.gov>
2015-02-03 22:11:25 +0000

    Header file cleanup
    git-svn-id: https://edclxs131.cr.usgs.gov:18080/svn/lsrd/lst/trunk@1120 48de96a7-6a0d-487c-b84a-40161698abc1

---
+++ not-validated-prototype-l5-7_lst/src/get_args.h
@@ -0,0 +1,21 @@
+#ifndef GET_ARGS_H
+#define GET_ARGS_H
+
+
+#include <stdbool.h>
+
+
+int get_args
+(
+    int argc,                /* I: number of cmd-line args */
+    char *argv[],            /* I: string of cmd-line args */
+    char *xml_infile,        /* I: address of input XML metadata filename  */
+    char *dem_infile,        /* I: address of input DEM filename */
+    char *emissivity_infile, /* I: address of input emissivity filename */
+    bool *tape_6,            /* O: use the tape6 output */
+    bool *verbose,           /* O: verbose flag */
+    bool *debug              /* O: debug flag */
+);
+
+
+#endif /* GET_ARGS_H */
