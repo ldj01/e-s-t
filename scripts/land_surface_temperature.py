cd888e09d70b45a43de341ec0b1a57c974f7c2bf
Ronald Dilley <rdilley@usgs.gov>
2015-06-04 14:46:02 -0500

    New file

---
+++ scripts/land_surface_temperature.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+
+import sys
+
+
+# ============================================================================
+if __name__ == '__main__':
+    '''
+    Description:
+      This doesn't do anything today, but will call the appropriate Land
+      Surface Temperature executable for the specified scene.
+    '''
+
+    sys.exit(1)  # Exit with failure, since we are not implemented yet.
