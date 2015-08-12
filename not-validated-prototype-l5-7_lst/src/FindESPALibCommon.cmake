4ae856be6b8942fcb281e3804e726385bee9d5e5
Ronald Dilley <rdilley@usgs.gov>
2015-08-12 16:59:02 -0500

    Updated

--- not-validated-prototype-l5-7_lst/src/FindESPALibCommon.cmake
+++ not-validated-prototype-l5-7_lst/src/FindESPALibCommon.cmake
@@ -27,14 +27,20 @@ find_library ( LibEspaFormatConversion_LIBRARY
     ENV ESPALIB
 )
 
-# TODO TODO TODO - Needs to be made smarter to report not finding the library
-
 set ( LibESPA_INCLUDES
         ${LibESPA_INCLUDE_DIR} )
 set ( LibESPA_LIBRARIES
         ${LibEspaRawBinary_LIBRARY}
         ${LibEspaCommon_LIBRARY}
         ${LibEspaFormatConversion_LIBRARY} )
+
+if ( NOT LibESPA_INCLUDES )
+  message( FATAL_ERROR "include directory for ESPA Common not found" )
+endif ()
+
+if ( NOT LibESPA_LIBRARIES )
+  message( FATAL_ERROR "lib directory for ESPA Common not found" )
+endif ()
 
 if ( LibESPA_INCLUDES )
   message( STATUS "Found ESPA Common Include Directory: ${LibESPA_INCLUDES}" )
