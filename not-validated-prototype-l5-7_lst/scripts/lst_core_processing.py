9e1af4d4317f5d5f2063f515a1bd6d9f132d4da0
Ronald Dilley <rdilley@usgs.gov>
2015-09-28 15:20:14 -0500

    Significant code cleanup related to other changes

--- not-validated-prototype-l5-7_lst/scripts/lst_core_processing.py
+++ not-validated-prototype-l5-7_lst/scripts/lst_core_processing.py
@@ -20,219 +20,20 @@
 import os
 import sys
 import shutil
-import glob
-import re
 import logging
 from argparse import ArgumentParser
-from datetime import datetime, timedelta
-
-# Import the metadata api found in the espa-product-formatter project
-import metadata_api
 
 # Import local modules
 import lst_utilities as util
+from lst_environment import Environment
 
+from extract_auxiliary_narr_data import AuxNARRGribProcessor
 import estimate_landsat_emissivity
 import build_lst_data
 
 
 # ============================================================================
-class AuxNARRGribProcessor(object):
-    '''
-    Description:
-        Extracts parameters from the auxillary NARR data in grib format and
-        places them into 'parameter' named directories.
-    '''
-
-    def __init__(self, xml_filename, base_aux_dir):
-        super(AuxNARRGribProcessor, self).__init__()
-
-        # Keep local copies of these
-        self.xml_filename = xml_filename
-
-        self.parms_to_extract = ['HGT', 'SPFH', 'TMP']
-        self.aux_path_template = '{0:0>4}/{1:0>2}/{2:0>2}'
-        self.aux_name_template = 'NARR_3D.{0}.{1:04}{2:02}{3:02}.{4:04}.{5}'
-
-        self.date_template = '{0:0>4}{1:0>2}{2:0>2}'
-        self.dir_template = base_aux_dir + '/{0}/{1}'
-
-        self.record_idx = 0
-        self.pressure_idx = 6
-
-        # Setup the logger to use
-        self.logger = logging.getLogger(__name__)
-
-    # ------------------------------------------------------------------------
-    def extract_grib_data(self, hdr_path, grb_path, output_dir):
-        '''
-        Description:
-            Configures a command line for calling the wgrib executable and
-            then calls it to extract the required information from the grib
-            file.
-
-            The output is placed into a specified directory based on the
-            input.
-        '''
-
-        util.System.create_directory(output_dir)
-
-        with open(hdr_path, 'r') as hdr_fd:
-            for line in hdr_fd.readlines():
-                self.logger.debug(line.strip())
-                parts = line.strip().split(':')
-                record = parts[self.record_idx]
-                pressure = parts[self.pressure_idx].split('=')[1]
-                self.logger.debug('{0} {1}'.format(record, pressure))
-
-                filename = '.'.join([pressure, 'txt'])
-                path = os.path.join(output_dir, filename)
-                cmd = ['wgrib', grb_path,
-                       '-d', record,
-                       '-text', '-o', path]
-                cmd = ' '.join(cmd)
-                self.logger.info('wgrib command = [{0}]'.format(cmd))
-
-                # Extract the pressure data and raise any errors
-                output = ''
-                try:
-                    output = util.System.execute_cmd(cmd)
-                except Exception:
-                    self.logger.error('Failed to unpack data')
-                    raise
-                finally:
-                    if len(output) > 0:
-                        self.logger.info(output)
-
-    # ------------------------------------------------------------------------
-    def extract_aux_data(self):
-        '''
-        Description:
-            Builds the strings required to locate the auxillary data in the
-            archive then extracts the parameters into parameter named
-            directories.
-        '''
-
-        xml = metadata_api.parse(self.xml_filename, silence=True)
-        global_metadata = xml.get_global_metadata()
-        acq_date = str(global_metadata.get_acquisition_date())
-        scene_center_time = str(global_metadata.get_scene_center_time())
-
-        # Extract the individual parts from the date
-        year = int(acq_date[:4])
-        month = int(acq_date[5:7])
-        day = int(acq_date[8:])
-
-        # Extract the hour parts from the time and convert to an int
-        hour = int(scene_center_time[:2])
-        self.logger.debug('Using Acq. Date = {0} {1} {2}'
-                          .format(year, month, day))
-        self.logger.debug('Using Scene Center Hour = {0:0>2}'.format(hour))
-
-        del (global_metadata)
-        del (xml)
-
-        # Determine the 3hr increments to use from the auxillary data
-        # We want the one before and after the scene acquisition time
-        # and convert back to formatted strings
-        hour_1 = hour - (hour % 3)
-        td = timedelta(hours=3)  # allows us to easily advance to the next day
-
-        date_1 = datetime(year, month, day, hour_1)
-        date_2 = date_1 + td
-        self.logger.debug('Date 1 = {0}'.format(str(date_1)))
-        self.logger.debug('Date 2 = {0}'.format(str(date_2)))
-
-        for parm in self.parms_to_extract:
-            # Build the source filenames for date 1
-            filename = self.aux_name_template.format(parm,
-                                                     date_1.year,
-                                                     date_1.month,
-                                                     date_1.day,
-                                                     date_1.hour * 100,
-                                                     'hdr')
-
-            aux_path = (self.aux_path_template.format(date_1.year,
-                                                      date_1.month,
-                                                      date_1.day))
-
-            hdr_1_path = self.dir_template.format(aux_path, filename)
-
-            grb_1_path = hdr_1_path.replace('.hdr', '.grb')
-
-            self.logger.info('Using {0}'.format(hdr_1_path))
-            self.logger.info('Using {0}'.format(grb_1_path))
-
-            # Build the source filenames for date 2
-            filename = self.aux_name_template.format(parm,
-                                                     date_2.year,
-                                                     date_2.month,
-                                                     date_2.day,
-                                                     date_2.hour * 100,
-                                                     'hdr')
-
-            aux_path = (self.aux_path_template.format(date_2.year,
-                                                      date_2.month,
-                                                      date_2.day))
-
-            hdr_2_path = self.dir_template.format(aux_path, filename)
-
-            grb_2_path = hdr_2_path.replace('.hdr', '.grb')
-
-            self.logger.info('Using {0}'.format(hdr_2_path))
-            self.logger.info('Using {0}'.format(grb_2_path))
-
-            # Verify that the files we need exist
-            if (not os.path.exists(hdr_1_path) or
-                    not os.path.exists(hdr_2_path) or
-                    not os.path.exists(grb_1_path) or
-                    not os.path.exists(grb_2_path)):
-                raise Exception('Required LST AUX files are missing')
-
-            # Date 1
-            output_dir = '{0}_1'.format(parm)
-            self.extract_grib_data(hdr_1_path, grb_1_path, output_dir)
-
-            # Date 2
-            output_dir = '{0}_2'.format(parm)
-            self.extract_grib_data(hdr_2_path, grb_2_path, output_dir)
-
-
-# ============================================================================
-def verify_environment():
-    '''
-    Description:
-        Verifies that all the required environment variables exist and
-        returns any used locally in the code.
-
-    Returns:
-        <string> : base LST auxiliary data directory
-    '''
-
-    # Not used here, only verified because a sub-executable requires it
-    tmp = os.environ.get('LST_DATA_DIR')
-    if tmp is None:
-        raise Exception('Missing environment variable LST_DATA_DIR')
-
-    # Not used here, only verified because a sub-executable requires it
-    tmp = os.environ.get('ASTER_GED_SERVER_NAME')
-    if tmp is None:
-        raise Exception('Missing environment variable ASTER_GED_SERVER_NAME')
-
-    # Used in here in this code
-    base_aux_dir = os.environ.get('LST_AUX_DIR')
-    if base_aux_dir is None:
-        raise Exception('Missing environment variable LST_AUX_DIR')
-
-    # Verify that the base_aux_dir directory exists
-    if not os.path.isdir(base_aux_dir):
-        raise Exception('LST_AUX_DIR directory does not exist')
-
-    return base_aux_dir
-
-
-# ============================================================================
-def generate_lst(xml_filename, base_aux_dir,
+def generate_lst(xml_filename,
                  only_extract_aux_data=False,
                  keep_lst_temp_data=False,
                  keep_intermediate_data=False,
@@ -249,7 +50,7 @@ def generate_lst(xml_filename, base_aux_dir,
     # Retrieval and initial processing of the required AUX data
     try:
         logger.info('Extracting LST AUX data')
-        current_processor = AuxNARRGribProcessor(xml_filename, base_aux_dir)
+        current_processor = AuxNARRGribProcessor(xml_filename)
         current_processor.extract_aux_data()
     except Exception:
         logger.error('Failed processing auxillary NARR data')
@@ -263,7 +64,6 @@ def generate_lst(xml_filename, base_aux_dir,
     # Extract the input ID from the xml filename and build some other
     # filenames
     input_id = os.path.splitext(xml_filename)[0]
-    mtl_filename = '{0}_MTL.txt'.format(input_id)
     # ESPA creates the DEM for us
     dem_filename = '{0}_dem.img'.format(input_id)
 
@@ -424,11 +224,11 @@ if __name__ == '__main__':
     logger = logging.getLogger(__name__)
 
     try:
-        base_aux_dir = verify_environment()
+        environment = Environment()
+        del environment
 
         logger.info('Generating LST products')
-
-        generate_lst(args.xml_filename, base_aux_dir,
+        generate_lst(args.xml_filename,
                      args.only_extract_aux_data,
                      args.keep_lst_temp_data,
                      args.keep_intermediate_data,
