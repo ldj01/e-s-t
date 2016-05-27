5935daf41abf7f05abe30086a0d2a58dd56a7aa4
Ronald Dilley <ronald.dilley.ctr@usgs.gov>
2016-05-27 15:17:06 -0500

    updated

--- not-validated-prototype_lst/scripts/lst_determine_grid_points.py
+++ not-validated-prototype_lst/scripts/lst_determine_grid_points.py
@@ -1,11 +1,10 @@
 #! /usr/bin/env python
 
 '''
-    File: lst_build_points.py
+    File: lst_determine_grid_points.py
 
-    Purpose: Builds a directory structure of points and required information
-             to be used for generation and interpolation of atmospheric
-             information by follow on applications.
+    Purpose: Determines a set of points some of which will be used to run MODTRAN
+             against.
 
     Project: Land Satellites Data Systems Science Research and Development
              (LSRD) at the USGS EROS
