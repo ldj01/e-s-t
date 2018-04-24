9c3c9a9dc041fae5d84d907e0bc5657b9c67ad76
Ray Dittmeier <ray.dittmeier.ctr@usgs.gov>
2018-04-24 15:49:59 -0500

    Add support for MERRA-2 in auxiliary scripts.

--- st_auxiliary_data/st_aux_http_session.py
+++ st_auxiliary_data/st_aux_http_session.py
@@ -1,4 +1,4 @@
-"""Provides a simplified Http Session class
+"""Provides simplified Http Session classes
 """
 
 import logging
@@ -8,6 +8,150 @@ from time import sleep
 
 
 logger = logging.getLogger(__name__)
+
+
+# Override requests.Session.rebuild_auth to maintain headers when redirected.
+class SessionWithHeaderRedirection(requests.Session):
+
+    def __init__(self, username, password, login_host,
+                 max_retries=3, block_size=None, timeout=300.0):
+        """Initialization of the class
+
+        Args:
+            username <str>: The username 
+            password <str>: The password 
+            login_host <str>: The name of the authorization host 
+            max_retries <int>: The number of max retries
+            block_size <int>: How many bytes to read at a time
+            timeout <float>: How long to wait before trying again
+        """
+    
+        super(SessionWithHeaderRedirection, self).__init__()
+        self.auth = (username, password)
+        global auth_host
+        auth_host = login_host
+
+        self.timeout = timeout
+        self.max_retries = max_retries
+        self.status_code = requests.codes['ok']
+
+        # Determine if we are streaming or not based on block_size
+        self.block_size = block_size
+        self.stream = False
+        if self.block_size is not None:
+            self.stream = True
+
+        adapter = requests.adapters.HTTPAdapter(max_retries=max_retries)
+        self.mount('http://', adapter)
+        self.mount('https://', adapter)
+
+    # Overrides from the library to keep headers when redirected to or from
+    # the NASA auth host.
+    def rebuild_auth(self, prepared_request, response):
+        headers = prepared_request.headers
+        url = prepared_request.url
+
+        if 'Authorization' in headers:
+            original_parsed = requests.utils.urlparse(response.request.url)
+            redirect_parsed = requests.utils.urlparse(url)
+
+            if (original_parsed.hostname != redirect_parsed.hostname) and \
+                    redirect_parsed.hostname != auth_host and \
+                    original_parsed.hostname != auth_host:
+                del headers['Authorization']
+        return
+
+    def _stream_file(self, download_url, destination_file, headers=None):
+        """Download by streaming 'block_size' of data at a time
+
+        Args:
+            download_url <str>: The URL to download
+            destination_file <str>: The file to create on the localhost
+            headers <str>: Any special headers to pass to the session
+        """
+
+        retrieved_bytes = 0
+        with closing(self.get(url=download_url,
+                          timeout=self.timeout,
+                          stream=True,
+                          headers=headers)) as req:
+            self.status_code = req.status_code
+
+            if not req.ok:
+                logger.error('HTTP - Transfer of [{0}] - FAILED'
+                             .format(download_url))
+                # The raise_for_status generates an exception to be caught
+                req.raise_for_status()
+
+            file_size = int(req.headers['content-length'])
+
+            # Set block size based on streaming
+            if self.stream:
+                block_size = self.block_size
+            else:
+                block_size = file_size
+
+            # Write the downloaded data to the destination file
+            with open(destination_file, 'wb') as local_fd:
+                for data_chunk in req.iter_content(block_size):
+                    local_fd.write(data_chunk)
+                    retrieved_bytes += len(data_chunk)
+
+            if retrieved_bytes != file_size:
+                raise Exception('Transfer Failed - HTTP -'
+                                ' Retrieved {0} out of {1} bytes'
+                                .format(retrieved_bytes, file_size))
+
+    def http_transfer_file(self, download_url, destination_file):
+        """Use http to transfer a file from a source location to a
+           destination file on the localhost
+
+        Args:
+            download_url <str>: The URL to download
+            destination_file <str>: The file to create on the localhost
+
+        Returns:
+            status_code - One of the following
+                        - 200, requests.codes['ok']
+                        - 404, requests.codes['not_found']:
+                        - 503, requests.codes['service_unavailable']:
+
+        Notes:
+            If a 503 is returned, the logged exception should be reviewed
+            to determine the real cause of the error.
+        """
+
+        logger.info(download_url)
+
+        retry_attempt = 0
+        done = False
+        while not done:
+            self.status_code = requests.codes['ok']
+            try:
+
+                self._stream_file(download_url, destination_file)
+
+                logger.info("Transfer Complete - HTTP")
+                done = True
+
+            except IOError:
+                logger.exception('HTTP - Transfer Issue')
+
+                if self.status_code not in (requests.codes['not_found'],
+                                            requests.codes['forbidden']):
+
+                    if retry_attempt > self.max_retries:
+                        logger.info('HTTP - Transfer Failed'
+                                    ' - exceeded retry limit')
+                        done = True
+                    else:
+                        retry_attempt += 1
+                        sleep(int(1.5 * retry_attempt))
+                else:
+                    # Not Found - So break the looping because we are done
+                    done = True
+
+        return self.status_code
 
 
 class HttpSession(object):
---
+++ st_auxiliary_data/st_aux_merra_config.py
@@ -0,0 +1,101 @@
+"""Provide configuration file reading
+"""
+
+
+import os
+from ConfigParser import ConfigParser
+from collections import namedtuple
+
+from st_aux_exception import AuxiliaryError
+
+
+class AuxiliaryConfigError(AuxiliaryError):
+    """Config errors
+    """
+    pass
+
+
+CONFIG_FILENAME = 'st_merra_auxiliary.conf'
+CONFIG_DIR = '.usgs/espa'
+
+
+def config_file_full_path():
+    """Build the full path to the config file
+
+    Raises:
+        AuxiliaryConfigError(message)
+    """
+
+    # Use the user's home directory as the base source directory for
+    # configuration
+    if 'HOME' not in os.environ:
+        raise AuxiliaryConfigError('[HOME] not found in environment')
+    home_dir = os.environ.get('HOME')
+
+    # Build the full path to the configuration file
+    config_path = os.path.join(home_dir, CONFIG_DIR, CONFIG_FILENAME)
+
+    return config_path
+
+
+def read_config():
+    """Reads a config file
+
+    Raises:
+        AuxiliaryConfigError(message)
+    """
+
+    config_path = config_file_full_path()
+
+    if not os.path.isfile(config_path):
+        raise AuxiliaryConfigError('Missing configuration file [{}]'
+                                    .format(config_path))
+
+    # Create the object and load the configuration
+    cfg = ConfigParser()
+    cfg.read(config_path)
+
+    return cfg
+
+
+NASAInfo = namedtuple('NASAInfo', ('data_url_format',
+                                   'data_name_format',
+                                   'login_url',
+                                   'action',
+                                   'username',
+                                   'password'))
+
+
+ConfigInfo = namedtuple('ConfigInfo', ('archive_directory_format',
+                                       'archive_name_format',
+                                       'base_archive_directory',
+                                       'search_date_range',
+                                       'transfer_block_size',
+                                       'nasa'))
+
+
+def get_config():
+    """Gets the configuration and converts it
+
+    Raises:
+        AuxiliaryConfigError(message)
+    """
+
+    cfg = read_config()
+
+    merra_section = 'merra2'
+    nasa_section = 'nasa'
+
+    nasa = NASAInfo(data_url_format=cfg.get(nasa_section, 'data_url_format'),
+                    data_name_format=cfg.get(nasa_section, 'data_name_format'),
+                    login_url=cfg.get(nasa_section, 'login_url'),
+                    action=cfg.get(nasa_section, 'action'),
+                    username=cfg.get(nasa_section, 'username'),
+                    password=cfg.get(nasa_section, 'password'))
+
+    return ConfigInfo(archive_directory_format=cfg.get(merra_section, 'archive_directory_format'),
+                      archive_name_format=cfg.get(merra_section, 'archive_name_format'),
+                      base_archive_directory=cfg.get(merra_section, 'base_archive_directory'),
+                      search_date_range=cfg.get(merra_section, 'search_date_range'),
+                      transfer_block_size=int(cfg.get(merra_section, 'transfer_block_size')),
+                      nasa=nasa)
