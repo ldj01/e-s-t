4e6f65987deb835620183c5ae4cb324d326e7715
Tammy Rockvam <tamara.rockvam.ctr@usgs.gov>
2018-08-08 12:31:19 -0500

    checkpoint on unit tests

--- provisional-st/scripts/unit-tests/st-tests.py
+++ provisional-st/scripts/unit-tests/st-tests.py
@@ -7,8 +7,20 @@ import unittest
 import subprocess
 import filecmp
 import shutil
+import sys
+
+# Import the ST modules that are not standalone scripts
+sys.path.insert(0, '..')
+import build_st_data
 
 class TestST(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(TestST, self).__init__(*args, **kwargs)
+        self.unit_test_data_dir = os.path.join(
+            os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
+            'espa-surface-temperature')
+
     def setUp(self):
         # The XML file for the scene we're working with
         self.xml_filename = 'LE07_L1TP_043028_20020419_20180206_01_T1.xml';
@@ -16,8 +28,7 @@ class TestST(unittest.TestCase):
         # List of files linked from ESPA_UNIT_TEST_DATA_DIR into the local
         # unit test directory, to use as inputs for a test
         self.link_files = [
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', self.xml_filename),
+            os.path.join(self.unit_test_data_dir, self.xml_filename),
             ]
         # List of output files and directories to compare to the expected
         # results in ESPA_UNIT_TEST_DATA_DIR
@@ -43,7 +54,9 @@ class TestST(unittest.TestCase):
     def run_test_case(self, cmd):
         status = subprocess.call(cmd)
         self.assertEqual(status, 0)
+        self.do_checks()
 
+    def do_checks(self):
         for check_dir in self.check_dirs:
             dc = filecmp.dircmp(check_dir, os.path.basename(check_dir))
             self.assertEqual(len(dc.diff_files), 0)
@@ -51,7 +64,7 @@ class TestST(unittest.TestCase):
             self.assertEqual(len(dc.common_funny), 0)
 
         for check_file in self.check_files:
-            print check_file
+            print "Comparing", check_file
             self.assertTrue(
                 filecmp.cmp(check_file, os.path.basename(check_file)))
 
@@ -65,15 +78,12 @@ class TestGridPoints(TestST):
         self.assertTrue(os.path.exists(self.data_path))
 
         self.link_files.extend(
-            glob.glob(os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', '*bt_band6*')))
+            glob.glob(os.path.join(self.unit_test_data_dir, '*bt_band6*')))
         self.setUpTestLinks()
 
         self.check_files = [
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'grid_points.bin'),
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'grid_points.hdr')
+            os.path.join(self.unit_test_data_dir, 'grid_points.bin'),
+            os.path.join(self.unit_test_data_dir, 'grid_points.hdr')
             ]
 
     def test_run(self):
@@ -93,18 +103,12 @@ class TestAuxMerra(TestST):
         self.setUpTestLinks()
 
         self.check_dirs = [
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'H_t0'),
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'H_t1'),
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'QV_t0'),
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'QV_t1'),
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'T_t0'),
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'T_t1')
+            os.path.join(self.unit_test_data_dir, 'H_t0'),
+            os.path.join(self.unit_test_data_dir, 'H_t1'),
+            os.path.join(self.unit_test_data_dir, 'QV_t0'),
+            os.path.join(self.unit_test_data_dir, 'QV_t1'),
+            os.path.join(self.unit_test_data_dir, 'T_t0'),
+            os.path.join(self.unit_test_data_dir, 'T_t1')
             ]
 
     def test_run(self):
@@ -119,40 +123,28 @@ class TestModtranInput(TestST):
         super(TestModtranInput, self).setUp()
 
         self.link_files.extend([
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'grid_points.bin'),
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'grid_points.hdr'),
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'H_t0'),
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'H_t1'),
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'QV_t0'),
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'QV_t1'),
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'T_t0'),
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'T_t1'),
+            os.path.join(self.unit_test_data_dir, 'grid_points.bin'),
+            os.path.join(self.unit_test_data_dir, 'grid_points.hdr'),
+            os.path.join(self.unit_test_data_dir, 'H_t0'),
+            os.path.join(self.unit_test_data_dir, 'H_t1'),
+            os.path.join(self.unit_test_data_dir, 'QV_t0'),
+            os.path.join(self.unit_test_data_dir, 'QV_t1'),
+            os.path.join(self.unit_test_data_dir, 'T_t0'),
+            os.path.join(self.unit_test_data_dir, 'T_t1'),
             ])
         self.link_files.extend(
-            glob.glob(os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'LE07*elevation*')))
+            glob.glob(os.path.join(self.unit_test_data_dir, 'LE07*elevation*')))
         self.setUpTestLinks()
 
         self.data_path = os.environ['ST_DATA_DIR']
         self.assertTrue(os.path.exists(self.data_path))
 
         self.check_dirs = glob.glob(
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', '00?_00?_???_???'))
+            os.path.join(self.unit_test_data_dir, '00?_00?_???_???'))
 
         self.check_files = [
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'grid_elevations.txt'),
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'modtran_elevations.txt')
+            os.path.join(self.unit_test_data_dir, 'grid_elevations.txt'),
+            os.path.join(self.unit_test_data_dir, 'modtran_elevations.txt')
             ]
 
     def test_run(self):
@@ -161,18 +153,16 @@ class TestModtranInput(TestST):
         self.run_test_case(cmd)
 
 class TestEmissivity(TestST):
-    """Test estimate_landsat_emissivity.py and 
-            estimate_landsat_emissivity_stdev.py """
+    """Test estimate_landsat_emissivity.py """
 
     def setUp(self):
         super(TestEmissivity, self).setUp()
 
         self.link_files.extend(
-            glob.glob(os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', '*bt_band6*')))
+            glob.glob(os.path.join(self.unit_test_data_dir, '*bt_band6*')))
         self.link_files.extend(
-            glob.glob(os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', '*toa_band[2345]*')))
+            glob.glob(os.path.join(self.unit_test_data_dir, 
+                '*toa_band[2345]*')))
         self.setUpTestLinks()
 
         self.aster_ged_server = os.environ['ASTER_GED_SERVER']
@@ -180,16 +170,18 @@ class TestEmissivity(TestST):
         # Do not test for existence of aster_ged_path, it may be a cloud bucket
 
         self.check_files = glob.glob(
-            os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'LE07*_emis*'))
+            os.path.join(self.unit_test_data_dir, 'LE07*_emis.*'))
         # Check the tif files from one ASTER granule
         self.check_files.extend(
-            glob.glob(os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', 'AG*tif')))
+            glob.glob(os.path.join(self.unit_test_data_dir, 'AG*emis.tif')))
+        self.check_files.extend(
+            glob.glob(os.path.join(self.unit_test_data_dir, 'AG*ndvi.tif')))
         # Intermediate files
         self.check_files.extend(
-            glob.glob(os.path.join(os.environ['ESPA_UNIT_TEST_DATA_DIR'], 
-                'espa-surface-temperature', '[a-z]*tif')))
+            glob.glob(os.path.join(self.unit_test_data_dir, '[a-z]*ndvi*.tif')))
+        self.check_files.extend(
+            glob.glob(os.path.join(self.unit_test_data_dir, 
+                'landsat_emis_[mw]*.tif')))
 
     def test_run(self):
         cmd = ["../estimate_landsat_emissivity.py", "--xml", self.xml_filename,
@@ -197,6 +189,190 @@ class TestEmissivity(TestST):
                "--aster-ged-server-name", self.aster_ged_server,
                "--aster-ged-server-path", self.aster_ged_path]
         self.run_test_case(cmd)
+        # Clean up any leftover ASTER files, not already in the check_files list
+        check_files_base = map(lambda x:os.path.basename(x), self.check_files)
+        for aster_file in glob.glob("AG100.v003*"):
+            if check_files_base.count(aster_file) > 0:
+                continue
+            os.unlink(aster_file)
+
+class TestEmissivityStdev(TestST):
+    """Test estimate_landsat_emissivity_stdev.py """
+
+    def setUp(self):
+        super(TestEmissivityStdev, self).setUp()
+
+        self.link_files.extend(
+            glob.glob(os.path.join(self.unit_test_data_dir, '*bt_band6*')))
+        self.link_files.extend(
+            glob.glob(os.path.join(self.unit_test_data_dir, '*toa_band3*')))
+        self.setUpTestLinks()
+
+        self.aster_ged_server = os.environ['ASTER_GED_SERVER']
+        self.aster_ged_path = os.environ['ASTER_GED_PATH']
+        # Do not test for existence of aster_ged_path, it may be a cloud bucket
+
+        self.check_files = glob.glob(
+            os.path.join(self.unit_test_data_dir, 'LE07*_emis_*'))
+        # Check the tif files from one ASTER granule
+        self.check_files.extend(
+            glob.glob(os.path.join(self.unit_test_data_dir, 
+                'AG*emis_stdev.tif')))
+        # Intermediate files
+        self.check_files.extend(
+            glob.glob(os.path.join(self.unit_test_data_dir, 
+                'landsat_emis_stdev*.tif')))
+
+    def test_run(self):
+        cmd = ["../estimate_landsat_emissivity_stdev.py", "--xml", 
+                self.xml_filename, "--intermediate",
+               "--aster-ged-server-name", self.aster_ged_server,
+               "--aster-ged-server-path", self.aster_ged_path]
+        self.run_test_case(cmd)
+        # Clean up any leftover ASTER files, not already in the check_files list
+        check_files_base = map(lambda x:os.path.basename(x), self.check_files)
+        for aster_file in glob.glob("AG100.v003*"):
+            if check_files_base.count(aster_file) > 0:
+                continue
+            os.unlink(aster_file)
+
+class TestModtran(TestST):
+    """Test st_run_modtran.py  """
+
+    def setUp(self):
+        super(TestModtran, self).setUp()
+
+        self.link_files.extend([
+            os.path.join(self.unit_test_data_dir, 'grid_points.bin'),
+            os.path.join(self.unit_test_data_dir, 'grid_points.hdr'),
+            ])
+        self.setUpTestLinks()
+
+        # Modtran output files are created in the point directories
+        # So set up special list of output files to check in the subdir
+        self.check_subdir_files = glob.glob(
+            os.path.join(self.unit_test_data_dir, '*', '*', '*', '*', 
+                'st_modtran.data'))
+        self.check_subdir_files.extend(
+            glob.glob(os.path.join(self.unit_test_data_dir, '*', '*', '*', '*', 
+                'st_modtran.hdr')))
+
+        # Modtran runs and creates output directly in the point directories
+        # So set up copies of them
+        self.copy_dirs = glob.glob(
+            os.path.join(self.unit_test_data_dir, '00?_00?_???_???'))
+        for d in self.copy_dirs:
+            shutil.copytree(d, os.path.basename(d))
+
+        self.modtran_data_path = os.environ['MODTRAN_DATA_DIR']
+        self.assertTrue(os.path.exists(self.modtran_data_path))
+
+        if os.environ.has_key('NUM_THREADS'):
+            self.num_threads = os.environ['NUM_THREADS']
+        else:
+            self.num_threads = 4
+
+    def tearDown(self):
+        super(TestModtran, self).tearDown()
+        # Delete extra directory copies
+        for d in self.copy_dirs:
+            shutil.rmtree(os.path.basename(d))
+
+    def test_run(self):
+        cmd = ["../st_run_modtran.py",
+               "--process_count", str(self.num_threads),
+               "--modtran_data_path", self.modtran_data_path]
+        self.run_test_case(cmd)
+
+        # Compare the output that was created in the point directories
+        for check_subdir_file in self.check_subdir_files:
+            local_subdir_file = check_subdir_file.replace(
+                os.path.join(self.unit_test_data_dir, ""))
+            print "Comparing", local_subdir_file
+            self.assertTrue(
+                filecmp.cmp(check_subdir_file, local_subdir_file));
+
+class TestAtmosParam(TestST):
+    """Test st_atmospheric_parameters (C code) """
+
+    def setUp(self):
+        super(TestAtmosParam, self).setUp()
+
+        self.data_path = os.environ['ST_DATA_DIR']
+        self.assertTrue(os.path.exists(self.data_path))
+
+        # This test modifies the XML file, so instead of linking to it, 
+        # make a copy.  Also remove it from the link_files list.
+        shutil.copyfile(
+            os.path.join(self.unit_test_data_dir, self.xml_filename), 
+            self.xml_filename)
+        self.link_files = []
+
+        self.link_files.extend(
+            glob.glob(os.path.join(self.unit_test_data_dir, 'LE07*b61*')))
+        self.link_files.extend(
+            glob.glob(os.path.join(self.unit_test_data_dir, 'LE07*elevation*')))
+        self.link_files.extend(
+            glob.glob(os.path.join(self.unit_test_data_dir, '00?_00?_???_???')))
+        self.link_files.extend([
+            os.path.join(self.unit_test_data_dir, 'grid_points.bin'),
+            os.path.join(self.unit_test_data_dir, 'grid_points.hdr'),
+            os.path.join(self.unit_test_data_dir, 'grid_elevations.txt'),
+            os.path.join(self.unit_test_data_dir, 'modtran_elevations.txt'),
+            ])
+        self.setUpTestLinks()
+
+        self.check_files = glob.glob(
+            os.path.join(self.unit_test_data_dir, 'LE07*_st_*'))
+        self.check_files.extend([os.path.join(self.unit_test_data_dir, 
+            'atmospheric_parameters.txt')])
+        self.check_files.extend([os.path.join(self.unit_test_data_dir, 
+            'used_points.txt')])
+
+    def test_run(self):
+        cmd = ["../../src/st_atmospheric_parameters", "--xml", 
+                self.xml_filename]
+        self.run_test_case(cmd)
+        # Compare the XML file with the expected one
+        # Ignore app_version and production_date
+        print "Comparing", self.xml_filename
+        diff_cmd = ["diff", "-I", "production_date", "-I", "app_version", 
+            self.xml_filename,
+            os.path.join(self.unit_test_data_dir, self.xml_filename + ".atmos")]
+        status = subprocess.call(diff_cmd)
+        self.assertEqual(status, 0)
+        os.unlink(self.xml_filename)
+
+class TestBuildSTData(TestST):
+    """Test build_st_data.py """
+
+    def setUp(self):
+        super(TestBuildSTData, self).setUp()
+
+        self.data_path = os.environ['ST_DATA_DIR']
+        self.assertTrue(os.path.exists(self.data_path))
+
+        # This test modifies the XML file, so instead of linking to it, 
+        # make a copy.  Also remove it from the link_files list.
+        shutil.copyfile(
+            os.path.join(self.unit_test_data_dir, self.xml_filename), 
+            self.xml_filename)
+        self.link_files = []
+
+        #self.link_files.extend(
+        #    glob.glob(os.path.join(self.unit_test_data_dir, '*bt_band6*')))
+        self.setUpTestLinks()
+
+        #self.check_files = [
+        #    os.path.join(self.unit_test_data_dir, 'grid_points.bin'),
+        #    os.path.join(self.unit_test_data_dir, 'grid_points.hdr')
+        #    ]
+
+    def test_run(self):
+        current_processor = build_st_data.BuildSTData(
+            xml_filename=self.xml_filename)
+        current_processor.generate_data()
+        self.do_checks()
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
